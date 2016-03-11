599877bfe861227bffbe24e2ea5b81cbbe05d37d
smcculley <smcculle@gmail.com>
2016-03-10 19:25:47 -0500

    week 7 exercise 1 commit

---
+++ week_7_exercise_1/src/com/sarah/BusinessContact.java
@@ -0,0 +1,18 @@
+package com.sarah;
+
+
+
+public class BusinessContact extends Contact{
+    private String phoneNumber;
+
+    BusinessContact(String name, String email, String phoneNumber){
+        super(name, email);
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void display(){
+        super.display();
+        System.out.println("Phone number " + phoneNumber);
+    }
+}
---
+++ week_7_exercise_1/src/com/sarah/Contact.java
@@ -0,0 +1,17 @@
+package com.sarah;
+
+
+public class Contact {
+    private String name;
+    private String email;
+
+    Contact(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public void display(){
+        System.out.println("Name: " + name);
+        System.out.println("Email " + email);
+    }
+}
